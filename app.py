dbc7c36b41895059eb1e8d1e02e4fe9b7bf72c80
tmwatchanan <tmwatchanan@gmail.com>
2018-07-18 23:07:05 +0700

    Add host and port parameters

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return '<h1>Hello World<h1/>'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=True)
