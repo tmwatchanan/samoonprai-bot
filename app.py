6a0c6aefe637db64e7bf438f7b3448ea7bd656c5
tmwatchanan <tmwatchanan@gmail.com>
2018-07-19 17:01:30 +0700

    Add interpretation from a request and send back as a response

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-from flask import Flask
+from flask import Flask, request, jsonify
+from rasa_core.interpreter import RasaNLUInterpreter
 
 app = Flask(__name__)
 
@@ -7,6 +8,13 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return '<h1>Hello World<h1/>'
+
+
+@app.route('/bot')
+def bot():
+    message = request.args.get('message')
+    interpreter = RasaNLUInterpreter("rasa/models/nlu/default/current")
+    return jsonify(interpreter.parse(message))
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
