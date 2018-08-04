bcc5623df1c7b2f89111781745aab55213b74144
tmwatchanan <tmwatchanan@gmail.com>
2018-08-04 15:29:44 +0700

    Add Docker

--- app.py
+++ app.py
@@ -61,5 +61,5 @@ def process(response):
 #     # return jsonify(interpreter.parse(message))
 
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True)
+# if __name__ == '__main__':
+#     app.run(host='127.0.0.1', port=5000, debug=True)
